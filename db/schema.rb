fb34bba036f5ecc452c04b1340e838cfb9217e9d
AlanDiasCW <dias.ferreira.alan@gmail.com>
2020-09-05 12:54:56 -0300

    Add user_id to documents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_145957) do
+ActiveRecord::Schema.define(version: 2020_09_05_154932) do
 
   create_table "documents", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
